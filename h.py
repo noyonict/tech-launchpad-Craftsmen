5872df8e42ba0c74ee331d26ff4a6c6e4b5daa67
Mohammad Kaium <mohammad.kaium@vivacomsolutions.com>
2023-08-26 21:45:00 +0600

    Functions:

---
+++ h.py
@@ -0,0 +1,10 @@
+# Functions:
+# Define a function called multiply. This function should take two parameters and return their product. Test your function with several pairs of numbers to ensure it works correctly.
+
+def multiply(a, b):
+    return a*b
+
+
+print(multiply(2.5,2))
+
+print(multiply(4,5))
