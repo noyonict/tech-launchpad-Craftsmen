b952b6f88b9aefd62b1de8f735531547f4ee7cbf
Mohammed Farhan Islam <farhanislam670@gmail.com>
2023-08-27 21:11:11 +0600

    Converted everything to .py files.

---
+++ mohammed-farhan-assignment/Q02.py
@@ -0,0 +1,4 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+
+print(f"Hello, {name}! You are {age} years old.")
---
+++ mohammed-farhan-assignment/Q08.py
@@ -0,0 +1,9 @@
+def multiplier(a, b):
+    return a * b
+
+
+# Test the function with different pairs of numbers
+print(multiplier(2, 3))   # Output: 6
+print(multiplier(5, -4))  # Output: -20
+print(multiplier(0, 7))   # Output: 0
+print(multiplier(-2, -2))  # Output: 4
