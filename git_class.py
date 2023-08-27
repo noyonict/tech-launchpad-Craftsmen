a5e97aa6959fbe68a245346958c360d078a2ee5b
noyon <noyonict@gmail.com>
2023-08-27 20:48:37 +0600

    added details how to submit the assignment to github

--- git_class.py
+++ git_class.py
@@ -22,3 +22,21 @@
 # git push --set-upstream origin <branch_name>
 # git clone <location>
 # git merge master
+
+# how to submit an assignment to Github
+
+# one time
+# git clone https://github.com/noyonict/tech-launchpad-Craftsmen.git
+# cd tech-launchpad-Craftsmen
+# git init
+# git checkout -b <yourname>_assignment
+
+# multiple time task
+# add your solutions and commit (add and commit)
+# add your solutions and commit (add and commit)
+# for adding - git add .
+# for commiting - git commit -m "useful commit message"
+# git push
+
+# last task
+# create a pull request to master branch.
