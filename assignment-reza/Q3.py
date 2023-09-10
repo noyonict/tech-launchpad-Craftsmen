adf74617a648ec9a3a9ce28b19dbc6a4617eef19
shams-ivon <sfurvdiorwen@gmail.com>
2023-09-10 15:05:43 +0600

    Revert "assignment added"
    This reverts commit 2586cd6f8b451b38b90e425bc0e013ef8ca43ed8.

--- assignment-reza/Q3.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Enter number: "))
-
-if num % 2 == 0:
-    print(f"the Number {num} is even")
-else:
-    print(f"the number {num} is odd")
--- assignment-reza/Q4.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter number: "))
-
-
-if num % 2 == 0 and num % 5 == 0:
-    print(f"This number, {num} is divisible by 2 and 5.")
-elif num % 2 == 0:
-    print(f"The number {num} is even")
-else:
-    print(f"The number, {num} is ODD")
